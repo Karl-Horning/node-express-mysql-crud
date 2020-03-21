881e93982b011f73e15301027bf34953b51f4902
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2020-03-21 00:56:54 +0000

    Database connection added

--- app.js
+++ app.js
@@ -4,8 +4,26 @@ const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const app = express();
+const config = require('./config');
 
 // App setup
+
+// Database connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: config.DB_USER,
+    password: config.DB_PASSWORD,
+    database: 'node_crud'
+});
+
+connection.connect((error) => {
+    if (!!error) {
+        console.log(error);
+    } else {
+        console.log('Database connected!');
+    }
+});
+
 
 // Routes
 app.get('/', (req, res) => {
